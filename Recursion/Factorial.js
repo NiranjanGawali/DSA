2fb7155d6677c604efd4e97dea4c0f889c94e795
Niranjan <gawaliniranjan@gmail.com>
2024-07-04 01:56:33 +0530

    Other folders deleted

--- Recursion/Factorial.js
+++
@@ -1,9 +0,0 @@
-function findFactorial(n) {
-  if (n === 1) return 1;
-  return n * findFactorial(n - 1);
-}
-
-let value = 4;
-let fact = findFactorial(value);
-
-console.log(`The factorial of ${value} is : ${fact}`);
